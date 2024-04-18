4a1d62c2ce674203384080ace4855f0611e3c695
Orcenhaze <orcenhaze@gmail.com>
2024-04-18 23:29:25 +0300

    vswhere

--- src/build.bat
+++ src/build.bat
@@ -3,6 +3,24 @@
 REM The app name that will be used for the executable. 
 set APPNAME=app
 
+
+REM Find latest Visual Studio installation via vswhere.exe to use MSVC toolset from command line.
+setlocal enabledelayedexpansion
+where /Q cl.exe || (
+  set __VSCMD_ARG_NO_LOGO=1
+  for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set "VS=%%i"
+  if "!VS!" equ "" (
+    echo ERROR: Visual Studio installation not found
+    exit /b 1
+  )  
+  call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
+)
+if "%VSCMD_ARG_TGT_ARCH%" neq "x64" (
+  echo ERROR: please run this from MSVC x64 native tools command prompt, 32-bit target is not supported!
+  exit /b 1
+)
+
+
 REM USED COMPILER OPTIONS:
 REM /Od      : Disables optimization.
 REM /MTd     : Links to the static version of the CRT (debug version).
